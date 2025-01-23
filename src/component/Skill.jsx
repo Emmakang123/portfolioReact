926a371a644e6e4b4202a37a5d0c9856bac3917f
Kang MinJoo <101373946+Emmakang123@users.noreply.github.com>
2025-01-22 21:05:20 -0800

    intro, skill section done

--- src/component/Skill.jsx
+++ src/component/Skill.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 
+const skillText=[
+    {
+        title :"",
+        desc :""
+    }
+]
 function Skill(){
-    return <div>Skill</div>
+    return (
+        <section id="skill">
+            <div className="skill__inner">
+                <h2 className="skill__title">Challenge<em>나의 도전</em></h2>
+                <div className="skill__desc">
+                    {skillText.map((skill,key) => (
+                        <div key={key}>
+                            <span>{key+1}.</span>
+                            <h3>{skill.title}</h3>
+                            <p>
+                                {skill.desc}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
 }
 export default Skill;
